4c5902e272d9e0d3aa57960aac37d7c3649fd6a3
Giulia Creps <giulia.creps2@gmail.com>
2026-05-02 12:15:25 +0200

    Initial Spring Boot project skeleton

---
+++ src/main/java/giuliacrepaldi/BStudent_Management/BStudentManagementApplication.java
@@ -0,0 +1,13 @@
+package giuliacrepaldi.BStudent_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BStudentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BStudentManagementApplication.class, args);
+	}
+
+}
